d4d3f72087e840cd7f1f2f4da1af8ad2fa40a46f
= <=>
2021-04-11 11:08:12 +0530

    add user model and url in ping controller

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,10 +1,10 @@
 import {inject} from '@loopback/core';
 import {
-  Request,
-  RestBindings,
-  get,
+  get, Request,
+
+
   response,
-  ResponseObject,
+  ResponseObject, RestBindings
 } from '@loopback/rest';
 
 /**
@@ -38,12 +38,24 @@ const PING_RESPONSE: ResponseObject = {
  * A simple controller to bounce back http requests
  */
 export class PingController {
-  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) { }
 
   // Map to `GET /ping`
   @get('/ping')
   @response(200, PING_RESPONSE)
   ping(): object {
+    // Reply with a greeting, the current time, the url, and request headers
+    return {
+      greeting: 'Hello from LoopBack',
+      date: new Date(),
+      url: this.req.url,
+      headers: Object.assign({}, this.req.headers),
+    };
+  }
+
+  @get('/testping')
+  @response(200, PING_RESPONSE)
+  testping(): object {
     // Reply with a greeting, the current time, the url, and request headers
     return {
       greeting: 'Hello from LoopBack',
---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,8 @@
+// import {inject} from '@loopback/context';
+// import {DataSourceType, DefaultCrudRepository} from '@loopback/repository';
+// import {User} from '../models';
+// export class UserRepository extends DefaultCrudRepository<User, string> {
+//   constructor(@inject('dataSources.memory') dataSource: DataSourceType) {
+//     super(User, dataSource);
+//   }
+// }
